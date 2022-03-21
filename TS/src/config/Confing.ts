a56dbb94cf1480d29d486478da5dfdb31588b288
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-03-21 02:06:47 +0200

    M99

--- TS/src/config/Confing.ts
+++ TS/src/config/Confing.ts
@@ -2,12 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    port: 1999,
-    urlHost: 'http://localhost:1999',
-    database: "mongodb+srv://mohamedmostafa:09705886877a@blogs.cq7zo.mongodb.net/Socket?retryWrites=true&w=majority"
-};
-/*
     port: process.env.port,
     urlHost: process.env.urlHost,
     database: process.env.database
+};
+/*
+    
 */
